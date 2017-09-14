97da48d42bd2c678cb856091f767b73e1d039352
Hiroki Osame <hiroki@weebly.com>
2017-09-14 00:21:39 -0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <test-component>
+    asdf
+  </test-component>
+</template>
+
+<script>
+import TestComponent from 'test-component';
+
+export default {
+  components: {
+    TestComponent,
+  }
+}
+</script>
